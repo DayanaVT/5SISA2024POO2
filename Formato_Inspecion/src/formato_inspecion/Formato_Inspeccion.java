d1103b063d25518a14e71b7a9286cad9209f1931
DayanaVT <d.villavicencio.t993@edu.utc.mx>
2024-02-15 22:38:59 -0600

    Examen POO

---
+++ Formato_Inspecion/src/formato_inspecion/Formato_Inspeccion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formato_inspecion;
+
+/**
+ *
+ * @author dayan
+ */
+public class Formato_Inspeccion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
